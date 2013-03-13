773b33baa3696ce68287df1704c8b88d012a5e50
Mark Burton <mark@greensocs.com>
2013-03-13 14:24:24 +0100

    Better environment variable handling, some cleanup in the version script, removal of the chef_fdt and apt cookbooks

--- versions/recipes/default.rb
+++ versions/recipes/default.rb
@@ -1,21 +1,36 @@
-#this chef scrpt is intended to pin LOCAL versions of specific resources where they are critical for
-#this specific package.
 
-#if the version of a package is not important, then it need not be listed here, 
+#  -------    VERSIONS  --------
 
-#apt versions can be pinned e.g.
-#apt_preference "mercurial" do
-#   pin "version 2.2.2-1"
-#   pin_priority "1001"
-#end
+# LICENSETEXT
+# 
+#   Copyright (C) 2013 : GreenSocs Ltd
+#       http://www.greensocs.com/ , email: info@greensocs.com
+# 
+# The contents of this file are subject to the licensing terms specified
+# in the file LICENSE. Please consult this file for restrictions and
+# limitations that may apply.
+# 
+# ENDLICENSETEXT
 
-#pining gcc to gcc-4.4
-package "gcc-4.4"
-package "g++-4.4"
-package "gcc-4.7"
-package "g++-4.7"
-package "gdb"
+
+# List of all the required packages, and below 'alternatives' is used to set the right versions of critical packages
+
+# If we are not running as root, with a /vagrant, then assume the user will set up their own environment
+if ENV['USER']=='root' && File.exists?("/vagrant")
+
+bash "Update package list" do
+code <<-EOH
+    for i in #{node[:prefix]}/bash.profile.d/*; do . $i; done
+    apt-get update
+EOH
+end
+
+#package "gcc-4.4"
+#package "g++-4.4"
+#package "gcc-4.7"
+#package "g++-4.7"
 package "build-essential"
+package "g++"
 
 
 package "mercurial"
@@ -37,6 +52,11 @@ package "libboost-program-options1.49-dev"
 package "libboost-regex1.49-dev"
 package "libboost-system1.49-dev"
 
+package "corkscrew"
+package "libprotobuf-dev"
+package "protobuf-compiler"
+package "libgoogle-perftools-dev"
+
 package "flex"
 package "bison"
 package "libncurses5-dev"
@@ -44,28 +64,44 @@ package "libglib2.0-dev"
 package "libpixman-1-dev"
 package "libsdl-dev"
 
+package "device-tree-compiler"
+package "libfdt-dev"
 
+package "default-jre"
+package "gdb"
 
-bash "Set up gcc alternatives" do
+#bash "Set up gcc alternatives" do
+#code <<-EOH
+#   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 10
+#   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 20
+#
+#   update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.7 10
+#   update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.4 20
+#
+#   update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+#   update-alternatives --set cc /usr/bin/gcc
+#
+#   update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+#   update-alternatives --set c++ /usr/bin/g++
+#
+#
+#    update-alternatives --config gcc
+#    update-alternatives --config g++
+#
+#
+#    update-alternatives --set gcc "/usr/bin/gcc-4.4"
+#    update-alternatives --set g++ "/usr/bin/g++-4.4"
+#EOH
+#end
+
+else puts "WARNING: Assuming the user has set up all the needed packages and versions"
+end
+
+bash "Set up source versions" do
 code <<-EOH
-   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 10
-   update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.4 20
-
-   update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.7 10
-   update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.4 20
-
-   update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
-   update-alternatives --set cc /usr/bin/gcc
-
-   update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
-   update-alternatives --set c++ /usr/bin/g++
-
-
-    update-alternatives --config gcc
-    update-alternatives --config g++
-
-
-    update-alternatives --set gcc "/usr/bin/gcc-4.4"
-    update-alternatives --set g++ "/usr/bin/g++-4.4"
+   mkdir -p "#{node[:prefix]}/bash.profile.d"
+   echo "export version_greenlib=HEAD" >  "#{node[:prefix]}/bash.profile.d/version_greenlib.profile"
 EOH
 end
+
+
